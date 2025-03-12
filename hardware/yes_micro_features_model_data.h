ff4e71f83055c3c0a98117bf190b8a96f44d5e56
Bazouz660 <basile.trebus-hamann@epitech.eu>
2025-03-12 14:04:32 +0100

    [DEL]: duplicate hardware folder

--- hardware/yes_micro_features_model_data.h
+++
@@ -1,7 +0,0 @@
-#ifndef YES_MICRO_FEATURES_MODEL_DATA_H_
-#define YES_MICRO_FEATURES_MODEL_DATA_H_
-
-extern const unsigned char g_yes_micro_features_model_data[];
-extern const int g_yes_micro_features_model_data_len;
-
-#endif  // YES_MICRO_FEATURES_MODEL_DATA_H_
